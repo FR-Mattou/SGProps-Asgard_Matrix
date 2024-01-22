3dce9b62a9c4d0446f35aacc584406222fd29718
FR-Mattou <fr-mattou@outlook.fr>
2024-01-22 22:19:48 +0100

    fix bug

--- lua/autorun/client/asgard_matrix_spawnmenu.lua
+++ lua/autorun/client/asgard_matrix_spawnmenu.lua
@@ -1,17 +1,28 @@
 local props = {
-    [1] = "models/sg-props/asgard_console_full.mdl",
-    [2] = "models/sg-props/asgard_crystal.mdl",
-    [3] = "models/sg-props/asgard_console_old.mdl",
-    [4] = "models/sg-props/asgard_console.mdl",
-    [5] = "models/sg-props/asgard_cart.mdl",
-    [6] = "models/sg-props/asgard_extra_basic.mdl",
-    [7] = "models/sg-props/asgard_extra_left.mdl",
-    [8] = "models/sg-props/asgard_extra_right.mdl"
+    "models/sg-props/asgard_console_full.mdl",
+    "models/sg-props/asgard_crystal.mdl",
+    "models/sg-props/asgard_console_old.mdl",
+    "models/sg-props/asgard_console.mdl",
+    "models/sg-props/asgard_cart.mdl",
+    "models/sg-props/asgard_extra_basic.mdl",
+    "models/sg-props/asgard_extra_left.mdl",
+    "models/sg-props/asgard_extra_right.mdl"
 }
 
 hook.Add( "PopulatePropMenu", "[Mattou]Asgard-Matrix_PropMenu", function()
     local contents = {}
-    contents = spawnmenu.GetPropTable()
+    
+    table.insert( contents, {
+	type = "header",
+	text = "Entities"
+    })
+
+    table.insert( contents, {
+        type = "entity",
+        spawnname = "asgard_console",
+        nicename = "Asgard Console",
+        material = "entities/asgard_console.png"
+    })
 
     table.insert( contents, {
         type = "header",
@@ -23,8 +34,8 @@ hook.Add( "PopulatePropMenu", "[Mattou]Asgard-Matrix_PropMenu", function()
             type = "model",
             model = props[i]
         })
-    end
+    end      
 
-    spawnmenu.AddPropCategory( "sg_props", "SG - Props", contents, "" )
+    spawnmenu.AddPropCategory( "sg_props_asgardmatrix", "SG-Props - Asgard Console", contents, "" )
 end)
 
